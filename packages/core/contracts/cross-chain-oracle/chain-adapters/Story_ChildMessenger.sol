1e477d76a018fd0f13365b82b4552b6b0df8754c
Spablob <tiagonunespereira@gmail.com>
2024-12-20 18:28:59 +0000

    new contracts boilerplate

---
+++ packages/core/contracts/cross-chain-oracle/chain-adapters/Story_ChildMessenger.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.26;
+
+/**
+ * @notice Sends and receives cross chain messages between Story L1 and Ethereum L1 network.
+ */
+contract Story_ChildMessenger {
+
+}
---
+++ packages/core/contracts/cross-chain-oracle/chain-adapters/Story_ParentMessenger.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.26;
+
+/**
+ * @notice Sends cross chain messages from Ethereum L1 to Story L1 network.
+ */
+contract Story_ParentMessenger {
+
+}
